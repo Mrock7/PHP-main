b1f0cf54368ba9669c2ea645a56b45fabb45901b
Mrock7 <guri.morina1@gmail.com>
2025-12-24 17:31:47 +0100

    m

---
+++ lesson9/addcolumn.php
@@ -0,0 +1,16 @@
+<?php
+
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=testdb",  "root",  "");
+
+    $sql = "ALTER TABLE products ADD email VARCHAR(255)";
+
+    $pdo->exec($sql);
+
+
+    echo "colum created successfully!";
+
+}catch (PDOException $e){
+    echo "error creating column". $e->getMessage();
+}
+?>
---
+++ lesson9/droptable.php
@@ -0,0 +1,20 @@
+<?php
+
+
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=testdb",  "root",  "");
+
+    $sql = "DROP TABLE products";
+
+    $pdo->exec($sql);
+
+
+    echo "Table dropped successfully!";
+
+}catch (PDOException $e){
+    echo  $e->getMessage();
+}
+
+
+
+?>
